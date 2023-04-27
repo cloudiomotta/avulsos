d7acae97593714812c2df2217da47b047a6e4c03
cloudiomotta <claudio.txmotta@gmail.com>
2023-04-27 12:51:15 -0300

    w

---
+++ arquivo_teste.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv("pop2000.csv", sep=";")
+df['Homens']/=-df["Homens"].sum()
+df["Mulheres"]/=df["Mulheres"].sum()
+ax=df.plot.barh(x="Idade",y="Homens", xlabel="Proporção Populacional", ylabel="Idade")
+df.plot.barh(x="Idade",y="Mulheres", color="Red",ax=ax)
+plt.show()
